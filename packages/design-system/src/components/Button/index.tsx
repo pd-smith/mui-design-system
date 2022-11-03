8e2150c12236d3d960edbc582ec8d4e757e250ea
pd-smith <pat@Patricks-MacBook-Air.local>
2022-11-02 20:08:11 -0400

    Show example of extending props

--- packages/design-system/src/components/Button/index.tsx
+++ packages/design-system/src/components/Button/index.tsx
@@ -1,1 +1,11 @@
-export { default as Button, type ButtonProps } from '@mui/material/Button';
+import React from 'react';
+import MuiButton, { type ButtonProps as MuiButtonProps } from '@mui/material/Button';
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+type CustomProps = {};
+
+export type ButtonProps<C extends React.ElementType> = MuiButtonProps<C, { component?: C } & CustomProps>;
+
+export function Button<C extends React.ElementType>(props: ButtonProps<C>) {
+    return <MuiButton {...props} />;
+}
